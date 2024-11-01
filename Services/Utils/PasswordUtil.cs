c64e906b0b1e69e42678f67293c8f5aa4efb8fda
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 03:45:46 +0800

    add HashPassword method

--- Services/Utils/PasswordUtil.cs
+++ Services/Utils/PasswordUtil.cs
@@ -7,6 +7,9 @@ namespace UserAuthentication_ASPNET.Services.Utils
 {
     public class PasswordUtil
     {
-        
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
     }
 }
