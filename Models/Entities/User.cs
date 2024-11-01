aa48a5320c7b4b725f5612b2fb4a79eaa458353b
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 02:51:42 +0800

    add User entity

---
+++ Models/Entities/User.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace UserAuthentication_ASPNET.Models.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public sealed class User : BaseEntity
+    {
+        public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Password { get; set; } = null!;
+    }
+}
