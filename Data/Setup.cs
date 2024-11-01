bfd953602267f5b5e0a04bea79a8fa878f9314bf
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 02:48:50 +0800

    configure email entity

---
+++ Data/Setup.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using UserAuthentication_ASPNET.Models.Entities;
+
+namespace UserAuthentication_ASPNET.Data
+{
+    public partial class DataContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => new
+                {
+                    u.Email
+                })
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
