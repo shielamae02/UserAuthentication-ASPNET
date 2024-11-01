96da58ce5d88ba6b37a330d034fbfd7c6f8244e9
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 02:47:15 +0800

    add user dbset to data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using UserAuthentication_ASPNET.Models.Entities;
+
+namespace UserAuthentication_ASPNET.Data
+{
+    public partial class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; init; }
+    }
+}
