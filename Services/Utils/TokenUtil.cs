c8b8577d5e405da12000881742896b4968c50f6b
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 02:59:25 +0800

    add TokenUtil class

---
+++ Services/Utils/TokenUtil.cs
@@ -0,0 +1,13 @@
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using UserAuthentication_ASPNET.Models.Entities;
+
+namespace UserAuthentication_ASPNET.Services.Utils
+{
+    public class TokenUtil
+    {
+
+    }
+}
