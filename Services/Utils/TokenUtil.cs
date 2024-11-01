1492abd0910d5a3a8469de67275e36a4246f87c2
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 03:00:59 +0800

    implement GenerateToken method

--- Services/Utils/TokenUtil.cs
+++ Services/Utils/TokenUtil.cs
@@ -8,6 +8,33 @@ namespace UserAuthentication_ASPNET.Services.Utils
 {
     public class TokenUtil
     {
+        private static string GenerateToken(User user, IConfiguration configuration, DateTime expires, bool isAccessToken = true)
+        {
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+            };
 
+            if (isAccessToken)
+            {
+                claims.Add(new(ClaimTypes.Email, user.Email));
+                claims.Add(new(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:Issuer"],
+                audience: configuration["JWT:Audience"],
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
