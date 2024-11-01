f12e328fb7d37a058af78c110de323481a0516b0
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 02:57:26 +0800

    add utility methods for generating error messages

--- Models/Utils/Error.cs
+++ Models/Utils/Error.cs
@@ -16,5 +16,30 @@ namespace UserAuthentication_ASPNET.Models.Utils
         public const string NotFound = "Resource not found";
         public const string AlreadyExists = "Already exists";
 
+        public static string FIELD_IS_REQUIRED(string field)
+        {
+            return $"{field} is required";
+        }
+
+        public static string ERROR_FETCHING_RESOURCE(string resource)
+        {
+            return $"Failed to fetch {resource}";
+        }
+
+        public static string ERROR_CREATING_RESOURCE(string resource)
+        {
+            return $"Failed to create {resource}";
+        }
+
+        public static string ERROR_UPDATING_RESOURCE(string resource)
+        {
+            return $"Failed to update {resource}";
+        }
+
+        public static string ERROR_DELETING_RESOURCE(string resource)
+        {
+            return $"Failed to delete {resource}";
+        }
+
     }
 }
