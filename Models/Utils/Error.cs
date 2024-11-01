8649e73183fb898af404df8a21021e15b8007efc
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 02:54:56 +0800

    add ErrorType enum

--- Models/Utils/Error.cs
+++ Models/Utils/Error.cs
@@ -2,6 +2,13 @@ namespace UserAuthentication_ASPNET.Models.Utils
 {
     public static class Error
     {
-
+        public enum ErrorType
+        {
+            ValidationError,
+            Unauthorized,
+            NotFound,
+            BadRequest,
+            InternalServerError
+        }
     }
 }
