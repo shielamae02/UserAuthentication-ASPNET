9523062998c3d2d56f29a3e6ce5c8b48e7efbb4c
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 23:45:08 +0800

    implement GetActionResultFromError method in ControllerUtil

--- Controllers/Utils/ControllerUtil.cs
+++ Controllers/Utils/ControllerUtil.cs
@@ -6,6 +6,19 @@ namespace UserAuthentication_ASPNET.Controllers.Utils
 {
     public static class ControllerUtil
     {
+        public static IActionResult GetActionResultFromError<T>(ApiResponse<T> apiResponse)
+        {
+            var errorType = apiResponse.ErrorType;
 
+            return errorType switch
+            {
+                ErrorType.NotFound => new NotFoundObjectResult(apiResponse),
+                ErrorType.ValidationError => new BadRequestObjectResult(apiResponse),
+                ErrorType.Unauthorized => new UnauthorizedObjectResult(apiResponse),
+                ErrorType.BadRequest => new BadRequestObjectResult(apiResponse),
+                ErrorType.InternalServerError => new StatusCodeResult(500),
+                _ => new BadRequestObjectResult(apiResponse)
+            };
+        }
     }
 }
