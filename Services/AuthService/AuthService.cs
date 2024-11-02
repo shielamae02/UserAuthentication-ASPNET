0bd64454a7278b4f175a39440bb7fa323ded15ce
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 23:15:13 +0800

    implement RegisterAsync method

--- Services/AuthService/AuthService.cs
+++ Services/AuthService/AuthService.cs
@@ -1,7 +1,11 @@
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using UserAuthentication_ASPNET.Data;
 using UserAuthentication_ASPNET.Models.Dtos;
+using UserAuthentication_ASPNET.Models.Entities;
 using UserAuthentication_ASPNET.Models.Response;
+using UserAuthentication_ASPNET.Models.Utils;
+using UserAuthentication_ASPNET.Services.Utils;
 
 namespace UserAuthentication_ASPNET.Services.AuthService
 {
@@ -12,6 +16,44 @@ namespace UserAuthentication_ASPNET.Services.AuthService
         ILogger<AuthService> logger
     ) : IAuthService
     {
+        public async Task<ApiResponse<AuthResponseDto>> RegisterAsync(AuthRegisterDto authRegister)
+        {
+            Dictionary<string, string> validationErrors = [];
 
+            await using var transaction = await context.Database.BeginTransactionAsync();
+            try
+            {
+                if (!authRegister.Password.Equals(authRegister.ConfirmPassword))
+                {
+                    validationErrors.Add("confirmPassword:", "Passwords do not match.");
+                    return ApiResponse<AuthResponseDto>.ErrorResponse(
+                        Error.ValidationError, Error.ErrorType.ValidationError, validationErrors);
+                }
+
+                if (await context.Users.AnyAsync(u => u.Email.Equals(authRegister.Email)))
+                {
+                    validationErrors.Add("email", "Invalid email address.");
+                    return ApiResponse<AuthResponseDto>.ErrorResponse(
+                        Error.ValidationError, Error.ErrorType.ValidationError, validationErrors);
+                }
+
+                var user = mapper.Map<User>(authRegister);
+                user.Password = PasswordUtil.HashPassword(authRegister.Password);
+
+                var authDto = TokenUtil.GenerateTokens(user, configuration);
+
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                return ApiResponse<AuthResponseDto>.SuccessResponse(Success.IS_AUTHENTICATED, authDto);
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                logger.LogError(ex, "An error occurred while registering the user.");
+                return ApiResponse<AuthResponseDto>.ErrorResponse(Error.ERROR_CREATING_RESOURCE("user"), Error.ErrorType.InternalServerError, validationErrors);
+            }
+        }
     }
 }
