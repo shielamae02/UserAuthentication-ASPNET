a6b21c25eba94a60c7f8c4ae2eb9a18f01891639
shielamae02 <shiela.mlepon@gmail.com>
2024-11-02 23:44:39 +0800

    add ControllerUtil class

---
+++ Controllers/Utils/ControllerUtil.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using UserAuthentication_ASPNET.Models.Response;
+using static UserAuthentication_ASPNET.Models.Utils.Error;
+
+namespace UserAuthentication_ASPNET.Controllers.Utils
+{
+    public static class ControllerUtil
+    {
+
+    }
+}
