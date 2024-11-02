fc1f8de93f88cd01f5eb539deb0d17fafef07842
shielamae02 <shiela.mlepon@gmail.com>
2024-11-03 03:27:08 +0800

    add ValidationUtil class

---
+++ Services/Utils/ValidationUtil.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using UserAuthentication_ASPNET.Models.Utils;
+using UserAuthentication_ASPNET.Models.Response;
+
+namespace UserAuthentication_ASPNET.Services.Utils
+{
+    public static class ValidationUtil
+    {
+
+    }
+}
