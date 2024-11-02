9aba0abc258aa286e4deafdf31fca1c032102c92
shielamae02 <shiela.mlepon@gmail.com>
2024-11-03 03:29:50 +0800

    implement ValidateFields method in ValidationUtil

--- Services/Utils/ValidationUtil.cs
+++ Services/Utils/ValidationUtil.cs
@@ -6,6 +6,33 @@ namespace UserAuthentication_ASPNET.Services.Utils
 {
     public static class ValidationUtil
     {
+        public static ApiResponse<T>? ValidateFields<T>(object obj, Dictionary<string, string> validationErrors)
+        {
+            var validationContext = new ValidationContext(obj);
+            var validationResults = new List<ValidationResult>();
 
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+
+            validationErrors.Clear();
+
+            if (!isValid)
+            {
+                foreach (var validationResult in validationResults)
+                {
+                    foreach (var memberName in validationResult.MemberNames)
+                    {
+                        validationErrors[memberName] = validationResult.ErrorMessage;
+                    }
+                }
+
+                return ApiResponse<T>.ErrorResponse(
+                    Error.ValidationError,
+                    Error.ErrorType.ValidationError,
+                    validationErrors
+                );
+            }
+
+            return null;
+        }
     }
 }
