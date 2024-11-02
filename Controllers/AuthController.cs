ade1828e6df76f891d0fc33dade265518edd66d6
shielamae02 <shiela.mlepon@gmail.com>
2024-11-03 00:15:28 +0800

    implement RegisterUser method in AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UserAuthentication_ASPNET.Controllers.Utils;
 using UserAuthentication_ASPNET.Models.Dtos;
 using UserAuthentication_ASPNET.Services.AuthService;
 
@@ -15,6 +16,28 @@ namespace UserAuthentication_ASPNET.Controllers
         IAuthService authService,
         ILogger<AuthController> logger) : ControllerBase
     {
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] AuthRegisterDto authRegister)
+        {
+            try
+            {
+                var response = await authService.RegisterAsync(authRegister);
+
+                if (response.Status.Equals("error"))
+                {
+                    logger.LogWarning("Registration attempt failed for email: {Email}", authRegister.Email);
+                    return ControllerUtil.GetActionResultFromError(response);
+                }
+
+                logger.LogInformation("Registration successful for email: {Email}", authRegister.Email);
+                return StatusCode(201, response);
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "Registration failed unexpectedly for email: {Email}.", authRegister.Email);
+                return Problem("An error occured while processing your request.");
+            }
+        }
 
     }
 }
