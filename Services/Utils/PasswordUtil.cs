e6f5fbdbf12f58111dd2b151766b9a8f481f5cb4
shielamae02 <shiela.mlepon@gmail.com>
2024-11-04 00:58:59 +0800

    implement VerifyPassword method in PasswordUtil

--- Services/Utils/PasswordUtil.cs
+++ Services/Utils/PasswordUtil.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace UserAuthentication_ASPNET.Services.Utils
+public class PasswordUtil
 {
-    public class PasswordUtil
+    public static string HashPassword(string password)
     {
-        public static string HashPassword(string password)
-        {
-            return BCrypt.Net.BCrypt.HashPassword(password);
-        }
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public static bool VerifyPassword(string hashPassword, string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashPassword);
     }
 }
