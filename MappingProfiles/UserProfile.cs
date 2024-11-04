c87098f0a3e03b5160cf5babe5b03f9f89bdbc46
shielamae02 <shiela.mlepon@gmail.com>
2024-11-05 03:26:53 +0800

    add UserProfile class

---
+++ MappingProfiles/UserProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using UserAuthentication_ASPNET.Models.Entities;
+using UserAuthentication_ASPNET.Models.Dtos.Users;
+
+namespace UserAuthentication_ASPNET.MappingProfiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+
+    }
+}
