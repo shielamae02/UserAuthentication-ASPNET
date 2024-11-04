ac2a4382955aa61871c6d363274db002c77c70d9
shielamae02 <shiela.mlepon@gmail.com>
2024-11-05 03:26:08 +0800

    implement GetUserDetails in UsersController

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -13,5 +13,29 @@ public class UsersController(
     ILogger<UsersController> logger
 ) : ControllerBase
 {
+    [Authorize]
+    [HttpGet("me")]
+    public async Task<IActionResult> GetUserDetails()
+    {
+        try
+        {
+            var userId = ControllerUtil.GetUserId(User);
 
+            if (userId == -1)
+            {
+                logger.LogWarning("Unauthorized access attempt detected for user ID: {UserId}.", userId);
+                return Unauthorized();
+            }
+
+            var response = await userService.GetUserDetailsAsync(userId);
+
+            logger.LogInformation("Successfully fetched user details for userId: {UserId}.", userId);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical(ex, "Failed to fetch user details.");
+            return Problem("An error occurred while processing your request.");
+        }
+    }
 }
