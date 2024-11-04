ed7565c570a92eb1b107a6166578440442c2690a
shielamae02 <shiela.mlepon@gmail.com>
2024-11-05 03:58:20 +0800

    add LogoutAsync method in IAuthService

--- Services/AuthService/IAuthService.cs
+++ Services/AuthService/IAuthService.cs
@@ -2,8 +2,10 @@ using UserAuthentication_ASPNET.Models.Dtos;
 using UserAuthentication_ASPNET.Models.Response;
 
 namespace UserAuthentication_ASPNET.Services.AuthService;
+
 public interface IAuthService
 {
     Task<ApiResponse<AuthResponseDto>> RegisterAsync(AuthRegisterDto authRegister);
     Task<ApiResponse<AuthResponseDto>> LoginAsync(AuthLoginDto authLogin);
+    Task<bool> LogoutAsync(string refreshToken);
 }
