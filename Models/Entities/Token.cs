53e2682c1254779d7da66d77ad2829a7d03b380b
shielamae02 <shiela.mlepon@gmail.com>
2024-11-05 03:57:12 +0800

    add Token entity

---
+++ Models/Entities/Token.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserAuthentication_ASPNET.Models.Entities;
+
+[Index(nameof(Refresh), IsUnique = true)]
+public class Token : BaseEntity
+{
+    [ForeignKey("User")]
+    public int UserId { get; init; }
+    public string Refresh { get; set; } = null!;
+    public bool isRevoked { get; set; } = false;
+    public DateTime Expiration { get; init; }
+
+    // navigation properties
+    public User User { get; set; } = null!;
+}
