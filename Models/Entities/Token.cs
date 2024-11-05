c5ac1db8d2c1e56a1441552b472ac1f62b2d9a77
shielamae02 <shiela.mlepon@gmail.com>
2024-11-05 15:42:55 +0800

    update IsRevoked property typo

--- Models/Entities/Token.cs
+++ Models/Entities/Token.cs
@@ -9,7 +9,7 @@ public class Token : BaseEntity
     [ForeignKey("User")]
     public int UserId { get; init; }
     public string Refresh { get; set; } = null!;
-    public bool isRevoked { get; set; } = false;
+    public bool IsRevoked { get; set; } = false;
     public DateTime Expiration { get; init; }
 
     // navigation properties
