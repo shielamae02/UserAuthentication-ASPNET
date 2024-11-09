7eb77186f06d3e01e0d53e4b3d2cdd94ab51e007
shielamae02 <shiela.mlepon@gmail.com>
2024-11-10 00:34:26 +0800

    implement SendEmailAsync method in EmailService

--- Services/Emails/EmailService.cs
+++ Services/Emails/EmailService.cs
@@ -25,5 +25,58 @@ public class EmailService(
 
     private readonly Smtp _smtp = smtpOptions.Value;
 
+    public async Task<bool> SendEmailAsync(IEnumerable<string> emails, string subject, string content)
+    {
+        logger.LogInformation("Sending email notification...");
 
+        var emailList = emails.ToList();
+
+        if (emailList.Count == 0)
+        {
+            logger.LogWarning("No email addresses were provided to send the notifications.");
+            return false;
+        }
+
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("no.reply.devservice@gmail.com", _smtp.Username));
+
+        foreach (var email in emailList)
+        {
+            message.To.Add(new MailboxAddress("", email));
+            logger.LogInformation($"Sending email to {email}");
+        }
+
+        message.Subject = subject;
+
+        var htmlBody = EmailTemplate.GetEmailTemplateForResetPassword(subject, content);
+
+        message.Body = new TextPart("html")
+        {
+            Text = htmlBody
+        };
+
+
+        var result = await RetryPolicy.ExecuteAsync(async () =>
+        {
+            // Send the email using MailKit
+            using var client = new SmtpClient();
+            try
+            {
+                await client.ConnectAsync(_smtp.Server, _smtp.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(_smtp.Username, _smtp.Password);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+
+                logger.LogInformation("Email sent successfully.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation("Failed to send email.");
+                return false;
+            }
+        });
+
+        return result;
+    }
 }
