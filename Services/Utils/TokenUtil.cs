506be671e96cd00662b3580ccd9cec69aa36119b
shielamae02 <shiela.mlepon@gmail.com>
2024-11-18 01:15:57 +0800

    refactor GenerateToken util

--- Services/Utils/TokenUtil.cs
+++ Services/Utils/TokenUtil.cs
@@ -7,10 +7,19 @@ using UserAuthentication_ASPNET.Models.Dtos;
 
 namespace UserAuthentication_ASPNET.Services.Utils;
 
+public enum TokenType
+{
+    REFRESH,
+    ACCESS,
+    RESET
+}
+
 public class TokenUtil
 {
-    private static string GenerateToken(User user, IConfiguration configuration, DateTime expires, bool isAccessToken = true, bool isPasswordResetToken = false)
+    private static string GenerateToken(User user, IConfiguration configuration, TokenType tokenType)
     {
+        var expires = DateTime.UtcNow;
+
         var claims = new List<Claim>
             {
                 new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
@@ -18,15 +27,27 @@ public class TokenUtil
                 new(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
             };
 
-        if (isAccessToken)
+        switch (tokenType)
         {
-            claims.Add(new(ClaimTypes.Email, user.Email));
-            claims.Add(new(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            case TokenType.REFRESH:
+                expires = DateTime.UtcNow.AddHours(Convert.ToDouble(configuration["JWT:RefreshTokenExpiry"]));
+                break;
+
+            case TokenType.ACCESS:
+                expires = DateTime.UtcNow.AddHours(Convert.ToDouble(configuration["JWT:AccessTokenExpiry"]));
+                claims.Add(new(ClaimTypes.Email, user.Email));
+                claims.Add(new(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                break;
+
+            case TokenType.RESET:
+                expires = DateTime.UtcNow.AddMinutes(10);
+                claims.Add(new("purpose", "reset-password"));
+                claims.Add(new(ClaimTypes.Email, user.Email));
+                break;
         }
-        else if (isPasswordResetToken)
-        {
-            claims.Add(new("purpose", "password-reset"));
-        }
+
+        claims.Add(new(JwtRegisteredClaimNames.Exp, new DateTimeOffset(expires).ToUnixTimeSeconds().ToString(),
+        ClaimValueTypes.Integer64));
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -58,7 +79,7 @@ public class TokenUtil
 
     public static string GeneratePasswordResetToken(User user, IConfiguration configuration)
     {
-        var expiry = DateTime.UtcNow.AddHours(1);
+        var expiry = DateTime.UtcNow.AddMinutes(15);
 
         return GenerateToken(user, configuration, expiry, isAccessToken: false, isPasswordResetToken: true);
     }
