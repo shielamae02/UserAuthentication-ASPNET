cee26ea8e53ab630a7792e16ca3b54a941946d9f
shielamae02 <shiela.mlepon@gmail.com>
2024-11-18 17:45:40 +0800

    update RemoveRevokedTokenAsync method logic

--- Services/Auth/AuthService.cs
+++ Services/Auth/AuthService.cs
@@ -168,15 +168,9 @@ public class AuthService(
 
     public async Task RemoveRevokedTokenAsync()
     {
-        var tokens = await context.Tokens
+        await context.Tokens
             .Where(t => t.IsRevoked || t.Expiration < DateTime.UtcNow)
-            .ToListAsync();
-
-        if (tokens.Count != 0)
-        {
-            context.Tokens.RemoveRange(tokens);
-            await context.SaveChangesAsync();
-        }
+            .ExecuteDeleteAsync();
     }
 
     public async Task<ApiResponse<string>> ForgotPasswordAsync(AuthForgotPasswordDto forgotPasswordDto)
