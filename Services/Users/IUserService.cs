bbf8932434766b51d5b2ecbd3a16702797afdd80
shielamae02 <shiela.mlepon@gmail.com>
2024-11-19 18:07:11 +0800

    add swagger docs to IUserService

--- Services/Users/IUserService.cs
+++ Services/Users/IUserService.cs
@@ -5,5 +5,12 @@ namespace UserAuthentication_ASPNET.Services.Users;
 
 public interface IUserService
 {
+    /// <summary>
+    ///     Fetches the details of the authenticated user.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>
+    ///     The details of the user.
+    /// </returns>
     public Task<ApiResponse<UserDetailsDto>> GetUserDetailsAsync(int id);
 }
